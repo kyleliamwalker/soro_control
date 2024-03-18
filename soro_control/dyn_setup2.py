7010d8b455579e63713ddefe4f2bb4e689972672
kyleliamwalker <walker12@live.co.uk>
2024-03-18 10:09:05 +0000

    id change

--- soro_control/dyn_setup2.py
+++ soro_control/dyn_setup2.py
@@ -22,8 +22,8 @@ class DynamixelConfig():
         #self.DXL_ID                          = [ 9 ]#
         # self.DXL_ID                         = [ 2, 5, 8, 11 ] # segment 2
         # self.DXL_ID                         = [ 1, 4, 7, 10 ]   # segment 1
-        # self.DXL_ID                         = [ 1, 4, 7, 10, 2, 5, 8, 11 ]    # both segments
-        self.DXL_ID                         = [ 13, 14, 15, 16, 10, 20, 11, 17 ]    # SHELL
+        self.DXL_ID                         = [ 1, 4, 7, 10, 2, 5, 8, 11 ]    # both segments
+        # self.DXL_ID                         = [ 13, 14, 15, 16, 10, 20, 11, 17 ]    # SHELL
         self.BAUDRATE                       = 57600             # Dynamixel default baudrate : 57600
         self.DEVICENAME                     = '/dev/ttyUSB0'    # Check which port is being used on your controller
                                                         # ex) Windows: "COM1"   Linux: "/dev/ttyUSB0" Mac: "/dev/tty.usbserial-*"
